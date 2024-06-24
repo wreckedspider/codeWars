e7297b5f0a2dbc509d6dfe061ef2e41ca1c394eb
hakase <patrick@novarededrogarias.com.br>
2024-06-24 16:27:33 -0300

    criando um gerador de gatinhos

---
+++ practicingWebTechnologies/calculadoraDeIdade/script.js
@@ -0,0 +1,17 @@
+let currDate = document.getElementById("currDate");
+let dateOfBirth = document.querySelector("#DOB");
+const CalcAge = document.getElementById("CalcAge");
+const displayAge = document.getElementById("displayAge");
+const Age= document.getElementById('age');
+var today = new Date();
+currDate.innerText=`A data de hoje é: ${today.toLocaleDateString('en-US')}`;
+
+CalcAge.addEventListener("click",()=>{
+	var birthDate = new Date(dateOfBirth.value);
+	var age = today.getFullYear() - birthDate.getFullYear();
+	var m = today.getMonth() - birthDate.getMonth();
+	if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+		age = age - 1;
+	}
+	displayAge.style.visibility="visible";
+	Age.innerText=`Você tem ${age} anos de idade.`});
---
+++ practicingWebTechnologies/contador/script.js
@@ -0,0 +1,4 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds")
